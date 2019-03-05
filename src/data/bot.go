26696593f4b4a5b6e95f83eeea498295c0489a48
Meron <mernat777@gmail.com>
2019-03-05 13:08:55 +0300

    Init Template

--- src/data/bot.go
+++ src/data/bot.go
@@ -1,8 +1,19 @@
 package data
 
-
-
-//Bot structure implements what a Bot should have as a Client Bot. 
-type Bot struct{
-	name	string `json:"name"`
+// Bot structure implements what a Bot should have as a Client Bot.
+type Bot struct {
+	id         int64  `json:"id"`
+	firstName string `json:"firstName"`
 }
+
+// Message is an object that will be sent to the Bot.
+
+
+// Chat is an object that tells the DRBot it's for a channel
+// type Chat struct{
+// 	id	int64 `json:"id"`
+// 	chatType string `json:"ChatType"`
+// }
+// type Message struct{
+
+// }
