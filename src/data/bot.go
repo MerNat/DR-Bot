60b43154e0218d0bc0ff0012493117f3e15a678e
Meron <mernat777@gmail.com>
2019-03-06 16:16:50 +0300

    fixes #2

--- src/data/bot.go
+++ src/data/bot.go
@@ -9,24 +9,12 @@ import (
 
 // Bot structure implements what a Bot should have as a Client Bot.
 type Bot struct {
-	// id            int64  `json:"id"`
 	FirstName  string `json:"firstName"`
 	Connection net.Conn
 }
 
 // Bots has every Bot inside it
 var Bots []Bot
-
-// Message is an object that will be sent to the Bot.
-
-// Chat is an object that tells the DRBot it's for a channel
-// type Chat struct{
-// 	id	int64 `json:"id"`
-// 	chatType string `json:"ChatType"`
-// }
-// type Message struct{
-
-// }
 
 // AddBot adds the Bot
 func (bot *Bot) AddBot() (err error) {
